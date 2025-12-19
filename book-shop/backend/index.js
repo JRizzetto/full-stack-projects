60e2477c3cff9b54ec85fb5c2b8b889a275bfdf9
JRizzetto <jeffersonrizzetto@gmail.com>
2025-12-18 22:05:51 -0300

    Projeto book-shop

---
+++ book-shop/backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Entendido123!",
+  database: "book_list",
+  port: 3306,
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hello this is the backend!");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been create sucessfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connect to backend!1");
+});
