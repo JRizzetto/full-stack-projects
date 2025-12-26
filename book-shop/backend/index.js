a95ec232b3b7369db05cf7c0df35c3b4c88125c4
JRizzetto <jeffersonrizzetto@gmail.com>
2025-12-26 11:43:49 -0300

    Configuração de variaveis do MYSQL ajustadas

--- book-shop/backend/index.js
+++ book-shop/backend/index.js
@@ -8,11 +8,11 @@ dotenv.config();
 const app = express();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 app.use(express.json());
