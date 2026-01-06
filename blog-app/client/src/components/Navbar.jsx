07e5b2e5cecbbcd66ddf87018bca796677d9a756
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-06 19:00:43 -0300

    Navbar, footer, home, single

--- blog-app/client/src/components/Navbar.jsx
+++ blog-app/client/src/components/Navbar.jsx
@@ -13,6 +13,28 @@ const Navbar = () => {
           <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=desing">
+            <h6>DESING</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>John</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
         </div>
       </div>
     </div>
--- blog-app/client/src/pages/Single.jsx
+++ blog-app/client/src/pages/Single.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+        </div>
+      </div>
+      <div className="manu">m</div>
+    </div>
+  );
 };
 
 export default Single;
