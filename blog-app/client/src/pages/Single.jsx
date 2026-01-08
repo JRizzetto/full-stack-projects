763a71533fceb582b9efc9110f87910234dd2bbd
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-08 19:35:39 -0300

    Finalizando o front-End

--- blog-app/client/src/pages/Single.jsx
+++ blog-app/client/src/pages/Single.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
@@ -17,9 +21,33 @@ const Single = () => {
             <span>John</span>
             <p>Posted 2 days ago</p>
           </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
         </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae
+          repudiandae et officiis cumque voluptates. Consequatur facere,
+          voluptatibus quibusdam ex ea veniam explicabo itaque facilis! Nihil
+          saepe eligendi a perspiciatis dolorem? Ex sit ipsam nisi vero quod
+          optio sed praesentium dicta aut aspernatur fugiat dignissimos,
+          pariatur facere, molestias animi maxime? Assumenda impedit expedita
+          fugit nisi animi aspernatur pariatur deleniti minima deserunt
+          doloribus tempore soluta accusamus perspiciatis, laboriosam, tenetur
+          atque beatae libero voluptatem harum mollitia amet accusantium
+          ducimus! Autem, cumque vero. Reprehenderit, quo similique harum
+          molestiae accusantium dicta assumenda beatae maxime consectetur, quia,
+          qui nemo quisquam dolores minima laudantium! Ratione neque,
+          reprehenderit voluptatibus laborum, perspiciatis est quo architecto
+          quis iste rem suscipit. Cum mollitia eum quo accusamus eos? Enim ullam
+          odio officiis.
+        </p>
       </div>
-      <div className="manu">m</div>
+      <Menu />
     </div>
   );
 };
