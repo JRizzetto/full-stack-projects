286918ae54169b4ea602a0129f70b7eac34b7f22
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-13 10:57:55 -0300

    Sessão de login finalizada

--- blog-app/api/index.js
+++ blog-app/api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import postRouts from "./routes/posts.js";
 import userRouts from "./routes/users.js";
 import authRouts from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRouts);
 app.use("/api/users", userRouts);
 app.use("/api/posts", postRouts);
