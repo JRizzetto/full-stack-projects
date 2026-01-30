bc0c1d10e86b966d21576428aade4d1a85c93ebd
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-30 12:43:28 -0300

    chore: built a connection that utilizes boths GET and POST

--- treinamento-full-stack-didatico/backend/server.js
+++ treinamento-full-stack-didatico/backend/server.js
@@ -1,15 +1,32 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const cors = require("cors");
 
-const returning = {
-  message: "Pong",
-};
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const db = [];
 
 app.get("/ping", (req, res) => {
-  res.json(returning);
+  res.json({
+    message: "Pong",
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is reunning ${port}`);
+app.post("/api/message", (req, res) => {
+  const { name, email, message } = req.body;
+  returnMessage = { name, email, message };
+
+  console.log(req.body);
+  db.push(returnMessage);
+  res.status(201).json({ ok: true, data: returnMessage });
+});
+
+app.get("/api/message", (req, res) => {
+  res.json(db);
+});
+
+app.listen(3000, () => {
+  console.log(`Server is reunning 3000`);
 });
